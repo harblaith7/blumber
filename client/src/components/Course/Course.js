d2e184952fb20149b11edc532284bff6c50952b6
Laith Harb <harblaith77@gmail.com>
2020-03-13 14:20:43 -0400

    Refactored code so that each course is a seperate component

---
+++ client/src/components/Course/Course.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import StarRatings from 'react-star-ratings';
+import './Course.scss'
+
+class Course extends Component {
+
+    constructor(props){
+        super(props)
+        this.courseRef = React.createRef()
+    }
+
+    render() {
+        return (
+            <div className="Course" id={this.props.course.courseId} ref={this.courseRef}>
+                <img src={this.props.course.thumbnail} alt="" className="Course__thumbnail"/>
+                <div className="Course__description-container">
+                    <h4 className="Course__course-title">{this.props.course.courseName}</h4>
+                    <StarRatings
+                        rating={parseFloat(this.props.course.courseInfo.courseRating)}
+                        starRatedColor="gold"
+                        starDimension="2rem"
+                        starSpacing="0.2rem"
+                        numberOfStars={5}
+                        name='rating'
+                    />
+                    <h5 className="Course__course-author">{this.props.course.courseInfo.courseAuthor}</h5>
+                    <h4 className="Course__course-price">{this.props.course.courseInfo.coursePrice}</h4>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Course;
--- client/src/components/CourseList/CourseList.js
+++ client/src/components/CourseList/CourseList.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './CourseList.scss';
 import axios from 'axios';
 import StarRatings from 'react-star-ratings';
+import Course from '../Course/Course'
+import { v4 as uuidv4 } from 'uuid';
 
 const API_URL = 'http://localhost:8080'
 
@@ -43,24 +45,7 @@ class CourseList extends Component {
 
     displayCourses = () => {
         return this.state.courseList.map(course => {
-            return (
-                <div className="CourseList__course-container">
-                    <img src={course.thumbnail} alt="" className="CourseList__thumbnail"/>
-                    <div className="CourseList__description-container">
-                        <h4 className="CourseList__course-title">{course.courseName}</h4>
-                        <StarRatings
-                            rating={parseFloat(course.courseInfo.courseRating)}
-                            starRatedColor="gold"
-                            starDimension="2rem"
-                            starSpacing="0.2rem"
-                            numberOfStars={5}
-                            name='rating'
-                        />
-                        <h5 className="CourseList__course-author">{course.courseInfo.courseAuthor}</h5>
-                        <h4 className="CourseList__course-price">{course.courseInfo.coursePrice}</h4>
-                    </div>
-                </div>
-            )
+            return <Course course={course} key={uuidv4()}/>
         })
     }
 
