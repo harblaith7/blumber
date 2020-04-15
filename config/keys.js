0775633f37182311a5369d26e7d3794d716ab18a
Laith Harb <harblaith77@gmail.com>
2020-04-15 07:26:17 -0400

    finished auth flow

--- config/keys.js
+++ config/keys.js
@@ -1,32 +1,9 @@
-module.exports = {
-    googleClientID: "897604259631-mogvtb4djo63pu5btivbq6sjsh5h9mdm.apps.googleusercontent.com",
-    googleClientSecret: "L2wCfODGDSNS2fIcse4PSHwF",
-    facebookClientID: "208509043572908",
-    facebookClientSecret: "da663d9f761e56a88baa76b2ab60e100",
-    mongoURI: "mongodb+srv://blumbercourse:yfnN9goryh2PoQ6Y@cluster0-ditva.mongodb.net/test?retryWrites=true&w=majority",
-    cookieKey: "My momma called, seen you on TV, son Said shit done changed I dreamed it all ever since I was They said I wouldn't be nothing Now they always say, 'Congratulations' (Uh, uh, uh)",
-    stripePublishableKey: "pk_test_oMT5cfppt04vWhvnFVzd4eRQ00nR1VMwO7",
-    stripeSecretKey: "sk_test_GgDixZgKuMuCXLH1TNanfQ5M00sECktZ3s"
+if(process.env.NODE_ENV === "production"){
+    module.exports = require("./prod")
+} else {
+    module.exports = require("./dev")
 }
 
-// GOOGLE CLIENT AND SECRET ARE FROM LAITHHARB777 AND MONGOURI IS FROM BLUMBERCOURSE
 
 
 
-
-/*
-module.exports = {
-    googleClientId: "8005092658-jshrlj30taoq24i7c47uvceq1o9jr051.apps.googleusercontent.com",
-    googleClientSecret: "Pnm1BS2gptcdToEi-a9bLlTr",
-    mongoURI: "mongodb+srv://laithharb:yfCOPSnz09lzwFPZ@blumber-mcbfi.mongodb.net/test?retryWrites=true&w=majority",
-    cookieKey: "My momma called, seen you on TV, son Said shit done changed I dreamed it all ever since I was They said I wouldn't be nothing Now they always say, 'Congratulations' (Uh, uh, uh)"
-}
-
-/*
-
-module.exports = {
-    googleClientId: "484255215753-lod4cdg44480orbt5qonmmmlrbm115vo.apps.googleusercontent.com",
-    googleClientSecret: "ia_Um6xSXZAQzkjDUb0fXDON"
-}
-
-*/
---
+++ config/prod.js
@@ -0,0 +1,18 @@
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    facebookClientID: "208509043572908",
+    facebookClientSecret: "da663d9f761e56a88baa76b2ab60e100",
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: "pk_test_oMT5cfppt04vWhvnFVzd4eRQ00nR1VMwO7",
+    stripeSecretKey: "sk_test_GgDixZgKuMuCXLH1TNanfQ5M00sECktZ3s"
+}
+
+
+/*
+mongodb+srv://laithharb:f1aXHXbzlAjYLLNV@blumber-prod-qf9wy.mongodb.net/test?retryWrites=true&w=majority
+631227845363-rc4v7507vu2np42hasa927ek9nk1qg1g.apps.googleusercontent.com
+esX5QxcJvFHG5tpaDNFg3ra5
+
+*/
